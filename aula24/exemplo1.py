325a1c6022427a1b9004e307dcdc9c4cafc42277
VisPatrick <pksantos1003@gmail.com>
2025-07-10 19:36:02 -0300

    Leitura do arquivo parquet com pandas

--- aula24/exemplo1.py
+++ aula24/exemplo1.py
@@ -1,4 +1,21 @@
-# A CRIAÇÃO DO ARQUIVO PARQUET É IMPORTANTE PARA PODER FAZER O PROCESSAMENTO,  ARQUIVO CSV NÃO É RECOMENDADO PARA PROCESSAMENTOM, É CONSIDERADO UM PRÉ-PROCESSAMENTO
+# A CRIAÇÃO DO ARQUIVO PARQUET É IMPORTANTE PARA PODER FAZER O PROCESSAMENTO,  ARQUIVO CSV NÃO É RECOMENDADO PARA PROCESSAMENTO, É CONSIDERADO UM PRÉ-PROCESSAMENTO
+# pip install pyarrow fastparquet # PARA INSTALAR O PARQUET
 
+from datetime import datetime
+import pandas as pd
+# import polars as pl
 
+ENDERECO_ARQUIVO = r'../../bronze/'
 
+try:
+    print('\nIniciando a leitura do arquivo Parquet...')
+    inicio = datetime.now()
+
+    df_bolsa_familia = pd.read_parquet(ENDERECO_ARQUIVO + 'bolsa_familia.parquet')
+    print(df_bolsa_familia.head())
+
+    fim = datetime.now()
+    print(f'\nLeitura do arquivo Parquet concluída em {fim - inicio} segundos.')
+    print('Arquivo Parquet lido com sucesso!')
+except Exception as e:
+    print(f'Erro ao ler o arquivo: {e}')
